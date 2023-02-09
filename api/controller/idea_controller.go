32e3995271c0b6f7734e092f4f2bccc2d4d2be77
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-02-09 22:11:11 +0100

    ideas: create empty controller.

--- api/controller/idea_controller.go
+++ api/controller/idea_controller.go
@@ -3,5 +3,11 @@ package controller
 import "github.com/epysqyli/anchors-backend/domain"
 
 type IdeaController struct {
-	IdeaUsecase domain.IdeaUsecase
+	IdeaRepository domain.IdeaRepository
 }
+
+// setup test for this?
+// 1 - call to endpoint
+// 2 - controller action
+// 3 - database actions via repository
+// 4 - json response
