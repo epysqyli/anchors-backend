63db5053252d185f4a9b5c3e9baca2ad28d4b4e7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-02-14 23:33:29 +0100

    tests: add failing ideas controller test.

--- api/controller/idea_controller.go
+++ api/controller/idea_controller.go
@@ -5,9 +5,3 @@ import "github.com/epysqyli/anchors-backend/domain"
 type IdeaController struct {
 	IdeaRepository domain.IdeaRepository
 }
-
-// setup test for this?
-// 1 - call to endpoint
-// 2 - controller action
-// 3 - database actions via repository
-// 4 - json response
---
+++ api/controller/test/idea_controller_test.go
@@ -0,0 +1,22 @@
+package controller
+
+import "testing"
+
+func TestFetchIdeas(t *testing.T) {
+	// gin, db := setup()
+	// create new ideas associated to the user
+
+	t.Run("byID", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("allUserIdeas", func(t *testing.T) {
+		t.Fail()
+	})
+
+	// teardown
+}
+
+func TestCreateIdea(t *testing.T) {
+	t.Skip()
+}
