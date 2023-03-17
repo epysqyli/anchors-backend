3b71e51e868618dccf5c844c7729fa3e0946529d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-03-17 21:38:28 +0100

    tags: create basic test structure.

--- api/controller/test/tags_controller_test.go
+++ api/controller/test/tags_controller_test.go
@@ -1,1 +1,17 @@
 package controller
+
+import "testing"
+
+func TestTags(t *testing.T) {
+	gin, db := setup()
+	_, user := signup(gin, db, sampleUser())
+
+	t.Run("Create", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Cleanup(func() {
+		cleanupDatabase(db)
+		cleanupUser(db, user.Name)
+	})
+}
