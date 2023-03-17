21eebf05e67eed729939fba077615f6f840a8b26
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-03-17 20:42:38 +0100

    tag: create idea repo interface.

--- domain/tag.go
+++ domain/tag.go
@@ -1,6 +1,15 @@
 package domain
 
+import "context"
+
 type Tag struct {
 	ID   uint   `gorm:"primarykey"`
 	Name string `json:"chapter" gorm:"not null;size:100"`
+}
+
+type TagRepository interface {
+	Create(c context.Context, tag *Tag) (Tag, error)
+	FetchById(c context.Context, ID string) (Tag, error)
+	FetchByName(c context.Context, name string) (Tag, error)
+	Delete(c context.Context, ID string) (Tag error)
 }
