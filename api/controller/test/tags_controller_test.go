80ca1774b6f289656723745d482d7202e43ff0e7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-03-18 00:09:02 +0100

    tags: add CRUD tests.

--- api/controller/test/tags_controller_test.go
+++ api/controller/test/tags_controller_test.go
@@ -2,10 +2,14 @@ package controller
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/epysqyli/anchors-backend/domain"
+	"gorm.io/gorm"
 )
 
 func TestTags(t *testing.T) {
@@ -27,24 +31,98 @@ func TestTags(t *testing.T) {
 		assertEqual(http.StatusCreated, tagRec.Code, t, "Tag should have been created")
 	})
 
-	t.Run("FetchAllTags", func(t *testing.T) {
-		t.Skip()
+	t.Run("FetchAll", func(t *testing.T) {
+		createTag(db, "architecture")
+		tagReq, err := http.NewRequest(http.MethodGet, "/v1/tags", bytes.NewReader([]byte{}))
+		if err != nil {
+			t.Fatalf("could not create request: %v\n", err)
+		}
+
+		tagReq.Header.Add("Content-Type", "application/json")
+		tagReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", authTokens.AccessToken))
+		tagRec := httptest.NewRecorder()
+
+		gin.ServeHTTP(tagRec, tagReq)
+		assertEqual(http.StatusOK, tagRec.Code, t)
+
+		tags := []domain.Tag{}
+		json.NewDecoder(tagRec.Body).Decode(&tags)
+		assertUnequal(0, len(tags), t, "Tags should have been fetched")
 	})
 
 	t.Run("FetchByID", func(t *testing.T) {
-		t.Skip()
+		someTag := createTag(db, "economics")
+		tagReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/v1/tags/%d", someTag.ID), bytes.NewReader([]byte{}))
+		if err != nil {
+			t.Fatalf("could not create request: %v\n", err)
+		}
+
+		tagReq.Header.Add("Content-Type", "application/json")
+		tagReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", authTokens.AccessToken))
+		tagRec := httptest.NewRecorder()
+
+		gin.ServeHTTP(tagRec, tagReq)
+		assertEqual(http.StatusOK, tagRec.Code, t)
+
+		tag := domain.Tag{}
+		json.NewDecoder(tagRec.Body).Decode(&tag)
+		assertUnequal(0, tag.ID, t, "Tag ID should be assigned")
 	})
 
 	t.Run("FetchByName", func(t *testing.T) {
-		t.Skip()
+		someTag := createTag(db, "finance")
+		tagReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/v1/tags/%s", someTag.Name), bytes.NewReader([]byte{}))
+		if err != nil {
+			t.Fatalf("could not create request: %v\n", err)
+		}
+
+		tagReq.Header.Add("Content-Type", "application/json")
+		tagReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", authTokens.AccessToken))
+		tagRec := httptest.NewRecorder()
+
+		gin.ServeHTTP(tagRec, tagReq)
+		assertEqual(http.StatusOK, tagRec.Code, t)
+
+		tag := domain.Tag{}
+		json.NewDecoder(tagRec.Body).Decode(&tag)
+		assertUnequal(0, tag.Name, t, "Tag Name should be assigned")
 	})
 
 	t.Run("DeletebyID", func(t *testing.T) {
-		t.Skip()
+		someTag := createTag(db, "del")
+		tagReq, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("/v1/tags/%d", someTag.ID), bytes.NewReader([]byte{}))
+		if err != nil {
+			t.Fatalf("could not create request: %v\n", err)
+		}
+
+		tagReq.Header.Add("Content-Type", "application/json")
+		tagReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", authTokens.AccessToken))
+		tagRec := httptest.NewRecorder()
+
+		gin.ServeHTTP(tagRec, tagReq)
+		assertEqual(http.StatusAccepted, tagRec.Code, t)
+
+		tag := domain.Tag{}
+		db.Where(&domain.Tag{Name: "del"}).First(&tag)
+		assertEqual(uint(0), tag.ID, t, "There should be no 'del' tag")
 	})
 
 	t.Cleanup(func() {
 		cleanupDatabase(db)
 		cleanupUser(db, user.Name)
 	})
+}
+
+func createTag(db *gorm.DB, name string) domain.Tag {
+	tag := domain.Tag{Name: name}
+	db.Create(&tag)
+
+	return tag
+}
+
+func fetchTag(db *gorm.DB, name string) *domain.Tag {
+	tag := &domain.Tag{}
+	db.Where(&domain.Tag{Name: name}).First(tag)
+
+	return tag
 }
--- domain/tag.go
+++ domain/tag.go
@@ -2,7 +2,7 @@ package domain
 
 type Tag struct {
 	ID   uint   `gorm:"primarykey"`
-	Name string `json:"chapter" gorm:"not null;size:100"`
+	Name string `json:"chapter" gorm:"not null;size:100;unique"`
 }
 
 type TagRepository interface {
