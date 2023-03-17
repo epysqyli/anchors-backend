e48e4584f6b1bf0e36d3c1a3a885eb6251a65f19
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-03-17 22:20:42 +0100

    tags: add all controller tests.

--- api/controller/test/tags_controller_test.go
+++ api/controller/test/tags_controller_test.go
@@ -10,6 +10,22 @@ func TestTags(t *testing.T) {
 		t.Skip()
 	})
 
+	t.Run("FetchAllTags", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("FetchByID", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("FetchByName", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("DeletebyID", func(t *testing.T) {
+		t.Skip()
+	})
+
 	t.Cleanup(func() {
 		cleanupDatabase(db)
 		cleanupUser(db, user.Name)
